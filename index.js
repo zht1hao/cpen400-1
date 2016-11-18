098681bb61693dad7f0230dac942aa3893bbc30e
zht1hao <zht1hao@users.noreply.github.com>
2016-11-17 21:33:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-app.get('/products', function(request, response) {
+app.get('/', function(request, response) {
 
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
